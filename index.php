a8a760826b976942dd4f0d62282b6b26cde06093
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2024-09-04 10:28:57 +0700

    PHP Date & Time

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php 
 echo "belajar php dan git";
 echo "Perubahan Pertama";
+
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
